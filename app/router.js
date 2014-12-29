813605c1142f0e01be706369097fdcacc43cf887
home <charly.palencia@koombea.com>
2014-12-29 08:16:00 -0500

    page 66

--- app/router.js
+++ app/router.js
@@ -6,7 +6,13 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('friends', function() { });
+  this.resource('friends', function() {
+    this.route('new');
+    this.route('show', {path: ':friend_id'}, function(){
+      this.resource('articles', function() {});
+    });
+    this.route('edit', {path: ':friend_id/edit'});
+  });
 });
 
 export default Router;
